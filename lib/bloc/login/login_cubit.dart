75e4e13b9d98f08b2c9b0a9b68cc69785f974599
SariyaAlZoubi <sariaalzoubi@gmail.com>
2023-08-04 20:27:37 +0300

    Modified The LoginCubit

--- lib/bloc/login/login_cubit.dart
+++ lib/bloc/login/login_cubit.dart
@@ -1,14 +1,34 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:untitled1/bloc/login/login_state.dart';
+import 'package:untitled1/model/login/login_model.dart';
+import 'package:untitled1/theme/colors.dart';
 
-import '../../theme/colors.dart';
-import 'login_state.dart';
-
+import '../../network/remote/http.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(InitialState());
 
   static LoginCubit get(context) => BlocProvider.of(context);
+
+  LoginModel? loginModel;
+
+  void login({required phoneNumber, required password}) {
+    emit(LoadingState());
+    HttpHelper.postData(
+            url: "login",
+            data: {"phone_number": phoneNumber, "password": password})
+        .then((value) {
+      loginModel = LoginModel.fromJson(jsonDecode(value.body));
+
+      emit(SuccessState(loginModel!));
+    }).catchError((onError) {
+      print(onError.toString());
+      emit(ErrorState());
+    });
+  }
 
   bool password = false;
   Icon passwordICon = const Icon(
