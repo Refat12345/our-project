22048231e7b435a3e288bd4ccaf93ce9d077a9e0
SariyaAlZoubi <sariaalzoubi@gmail.com>
2023-08-04 20:26:10 +0300

    Modified The LoginScreen

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,8 +1,12 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
-import 'package:project2/page/register.dart';
-import 'package:project2/page/user/cart.dart';
-
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:untitled1/bloc/login/login_cubit.dart';
+import 'package:untitled1/bloc/login/login_state.dart';
+import 'package:untitled1/network/local/cache.dart';
+import 'package:untitled1/page/register.dart';
+import 'package:untitled1/page/user/cart.dart';
+import 'package:untitled1/page/user/product_details.dart';
 import '../component/helper.dart';
 import '../component/widget.dart';
 import '../theme/colors.dart';
@@ -24,108 +28,134 @@ class Login extends StatelessWidget {
             right: heightScreen / 45,
             top: heightScreen / 16),
         child: LayoutBuilder(builder: (context, constrain) {
-          onTap() {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => const Cart()));
-          }
-
           double fontSize = constrain.maxHeight / 50;
           return SingleChildScrollView(
             physics: const BouncingScrollPhysics(),
             child: Center(
               child: Form(
                 key: formKey,
-                child: Column(
-                  textDirection: TextDirection.rtl,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Center(
-                        child: SizedBox(
-                      height: constrain.maxHeight / 2.75,
-                      width: constrain.maxWidth / 1.2,
-                      child: getSvgIcon("welcome3.svg"),
-                    )),
-                    SizedBox(
-                      height: constrain.maxHeight / 20,
-                    ),
-                    Text(
-                      "رقم الهاتف",
-                      style: TextStyle(
-                          fontSize: fontSize,
-                          color: grey,
-                          fontWeight: FontWeight.w500,
-                          fontFamily: "Cairo"),
-                      textAlign: TextAlign.right,
-                    ),
-                    SizedBox(
-                      height: constrain.maxHeight / 70,
-                    ),
-                    Filed(
-                        controller: phoneNumber,
-                        hintText: "أدخل رقم الهاتف",
-                        height: constrain.maxHeight,
-                        login: true),
-                    SizedBox(
-                      height: constrain.maxHeight / 35,
-                    ),
-                    Text(
-                      "كلمة المرور",
-                      style: TextStyle(
-                          fontSize: fontSize,
-                          color: grey,
-                          fontWeight: FontWeight.w500,
-                          fontFamily: "Cairo"),
-                    ),
-                    SizedBox(
-                      height: heightScreen / 70,
-                    ),
-                    Filed(
-                        controller: password,
-                        hintText: "أدخل كلمة المرور",
-                        height: constrain.maxHeight,
-                        login: true),
-                    SizedBox(
-                      height: constrain.maxHeight / 30,
-                    ),
-                    Bottom(
-                      text: "تسجيل الدخول",
-                      height: constrain.maxHeight,
-                      onTap: onTap,
-                    ),
-                    SizedBox(
-                      height: constrain.maxHeight / 30,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                child: BlocProvider(
+                  create: (context) => LoginCubit(),
+                  child: BlocConsumer<LoginCubit, LoginState>(
+                      builder: (context, state) {
+                    onTap() {
+                      if (formKey.currentState!.validate()) {
+                        LoginCubit.get(context).login(
+                            phoneNumber: phoneNumber.text,
+                            password: password.text);
+                      }
+                    }
+
+                    return Column(
                       textDirection: TextDirection.rtl,
-                      children: [
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Center(
+                            child: SizedBox(
+                          height: constrain.maxHeight / 2.75,
+                          width: constrain.maxWidth / 1.2,
+                          child: getSvgIcon("welcome3.svg"),
+                        )),
+                        SizedBox(
+                          height: constrain.maxHeight / 20,
+                        ),
                         Text(
-                          "ليس لديك حساب بعد؟",
+                          "رقم الهاتف",
                           style: TextStyle(
                               fontSize: fontSize,
-                              decoration: TextDecoration.underline,
+                              color: grey,
+                              fontWeight: FontWeight.w500,
                               fontFamily: "Cairo"),
                           textAlign: TextAlign.right,
                         ),
-                        TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => Register()));
-                            },
-                            child: Text(
-                              " أنشئ حساب",
+                        SizedBox(
+                          height: constrain.maxHeight / 70,
+                        ),
+                        Filed(
+                            controller: phoneNumber,
+                            hintText: "أدخل رقم الهاتف",
+                            height: constrain.maxHeight,
+                            login: true),
+                        SizedBox(
+                          height: constrain.maxHeight / 35,
+                        ),
+                        Text(
+                          "كلمة المرور",
+                          style: TextStyle(
+                              fontSize: fontSize,
+                              color: grey,
+                              fontWeight: FontWeight.w500,
+                              fontFamily: "Cairo"),
+                        ),
+                        SizedBox(
+                          height: heightScreen / 70,
+                        ),
+                        Filed(
+                            controller: password,
+                            hintText: "أدخل كلمة المرور",
+                            height: constrain.maxHeight,
+                            login: true),
+                        SizedBox(
+                          height: constrain.maxHeight / 30,
+                        ),
+                        ConditionalBuilder(
+                          condition: state is! LoadingState,
+                          builder: (context) => Bottom(
+                            text: "تسجيل الدخول",
+                            height: constrain.maxHeight,
+                            onTap: onTap,
+                          ),
+                          fallback: (context) =>
+                              const Center(child: CircularProgressIndicator()),
+                        ),
+                        SizedBox(
+                          height: constrain.maxHeight / 30,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          textDirection: TextDirection.rtl,
+                          children: [
+                            Text(
+                              "ليس لديك حساب بعد؟",
                               style: TextStyle(
-                                  fontWeight: FontWeight.bold,
                                   fontSize: fontSize,
-                                  color: primary,
+                                  decoration: TextDecoration.underline,
                                   fontFamily: "Cairo"),
                               textAlign: TextAlign.right,
-                            ))
+                            ),
+                            TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Register()));
+                                },
+                                child: Text(
+                                  " أنشئ حساب",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: fontSize,
+                                      color: primary,
+                                      fontFamily: "Cairo"),
+                                  textAlign: TextAlign.right,
+                                ))
+                          ],
+                        )
                       ],
-                    )
-                  ],
+                    );
+                  }, listener: (context, state) {
+                    if (state is SuccessState) {
+                      if (state.loginModel.status == true) {
+                        CacheHelper.saveData(key: "token", value: state.loginModel.token);
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const ProductDetails()));
+                      } else {
+                        flutterToast(state.loginModel.message, "error");
+                      }
+                    }
+                  }),
                 ),
               ),
             ),
