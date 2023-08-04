a7d1a6c944f1f1b70cd92954c9b1d3bdc9959db7
SariyaAlZoubi <sariaalzoubi@gmail.com>
2023-08-04 20:28:16 +0300

    Modified The LoginState

--- lib/bloc/login/login_state.dart
+++ lib/bloc/login/login_state.dart
@@ -1,10 +1,15 @@
+import 'package:untitled1/model/login/login_model.dart';
+
 abstract class LoginState {}
 
 class InitialState extends LoginState {}
 
 class LoadingState extends LoginState {}
 
-class SuccessState extends LoginState {}
+class SuccessState extends LoginState {
+  final LoginModel loginModel;
+  SuccessState(this.loginModel);
+}
 
 class ErrorState extends LoginState {}
 
