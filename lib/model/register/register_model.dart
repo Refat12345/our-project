2ed659c33214252e9429f946242338676bf838f1
SariyaAlZoubi <sariaalzoubi@gmail.com>
2023-08-18 10:32:34 +0300

    upload register_model

---
+++ lib/model/register/register_model.dart
@@ -0,0 +1,45 @@
+class RegisterModel {
+  bool? status;
+  String? message;
+  Data? data;
+
+  RegisterModel({this.status, this.message, this.data});
+
+  RegisterModel.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    message = json['message'];
+    data = json['data'] != null ? Data.fromJson(json['data']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['status'] = status;
+    data['message'] = message;
+    if (this.data != null) {
+      data['data'] = this.data!.toJson();
+    }
+    return data;
+  }
+}
+
+class Data {
+  String? name;
+  String? phoneNumber;
+  int? id;
+
+  Data({this.name, this.phoneNumber, this.id});
+
+  Data.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    phoneNumber = json['phone_number'];
+    id = json['id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['name'] = name;
+    data['phone_number'] = phoneNumber;
+    data['id'] = id;
+    return data;
+  }
+}
