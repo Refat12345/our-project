9372354076752210ddc6eeff36fbc714f31de7ce
SariyaAlZoubi <sariaalzoubi@gmail.com>
2023-08-18 10:33:43 +0300

    update http

--- lib/network/remote/http.dart
+++ lib/network/remote/http.dart
@@ -1,19 +1,19 @@
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
+import 'package:untitled1/component/helper.dart';
 
 import '../local/cache.dart';
 
 class HttpHelper {
-
   static Future<Response> getData({required url}) async {
-    return await http.get(Uri.parse(url), headers: {
+    return await http.get(Uri.parse(EndPoint.url + url), headers: {
       'Accept': 'application/json',
       'Authorization': 'Bearer ${CacheHelper.getData(key: 'token')}'
     });
   }
 
   static Future<Response> postData({required url, required data}) async {
-    return await http.post(Uri.parse(url), body: data, headers: {
+    return await http.post(Uri.parse(EndPoint.url + url), body: data, headers: {
       'Accept': 'application/json',
       'Authorization': 'Bearer ${CacheHelper.getData(key: 'token')}'
     });
@@ -21,17 +21,19 @@ class HttpHelper {
 
   static Future<Response> deleteData({
     required url,
+    data
   }) async {
-    return await http.delete(Uri.parse(url), headers: {
+    return await http.delete(Uri.parse(EndPoint.url + url), headers: {
       'Accept': 'application/json',
       'Authorization': 'Bearer ${CacheHelper.getData(key: 'token')}'
-    });
+    },body: data);
   }
 
   static Future<Response> putData({required url, required data}) async {
-    return await http.put(Uri.parse(url), body: data, headers: {
+    return await http.put(Uri.parse(EndPoint.url + url), body: data, headers: {
       'Accept': 'application/json',
       'Authorization': 'Bearer ${CacheHelper.getData(key: 'token')}'
-    });
+    }
+    );
   }
 }
