ce053a1f7c15831551b36eea62d132c48003851e
SariyaAlZoubi <sariaalzoubi@gmail.com>
2023-08-21 00:38:26 +0300

    upload rating_product_bloc

---
+++ lib/bloc/rating_product/rating_product_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:untitled1/bloc/rating_product/rating_product_state.dart';
+import 'package:untitled1/network/remote/http.dart';
+
+class RatingProductCubit extends Cubit<RatingProductState>
+{
+  
+  RatingProductCubit():super(InitialState());
+  
+  static RatingProductCubit get(context)=>BlocProvider.of(context);
+
+  double rating=0;
+  Future ratingProduct ({required id})async
+  {
+    emit(LoadingState());
+    await HttpHelper.postData(url: 'addProductEvaluation/$id/$rating', data: {}).then((value)
+    {
+      emit(SuccessState());
+    }).catchError((onError)
+    {
+      print(onError.toString());
+      emit(ErrorState());
+    });
+  }
+}
